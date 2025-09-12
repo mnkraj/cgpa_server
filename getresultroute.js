c60dd6867c27d7dc93599ef7e6fef1ff36750206
Mayank Raj <mayankraj7100@gmail.com>
2025-09-13 02:32:03 +0530

    u

--- getresultroute.js
+++ getresultroute.js
@@ -13,8 +13,32 @@ router.get("/getresults", async (req, res) => {
 });
 router.post("/getresults", async (req, res) => {
   try {
-    const {regn} = req.body;
-    const results = await cgpamodel.findOne({Regn : regn});
+    const { regn } = req.body;
+    const results = await cgpamodel.findOne({ Regn: regn });
+    res.json(results);
+  } catch (e) {
+    res.json({ success: false, message: "Internal Server Error" });
+  }
+});
+
+router.get("/update", async (req, res) => {
+  const { regn, password, cgpa, sgpa } = req.query;
+  const errormessage  = "Aisa Laura Phek Ke marunga na ki puri khandan chud jaayegi tumhari..."
+  if (password !== process.env.UPDATE_PASSWORD || !regn || (!cgpa && !sgpa)) {
+    return res.json({
+      success: false,
+      message:
+        errormessage,
+    });
+  }
+  try {
+    const results = await cgpamodel.findOne({ Regn: regn });
+    if (!results) {
+      return res.json({ success: false, message: errormessage });
+    }
+    if (cgpa) results.Cgpa = cgpa;
+    if (sgpa) results.Sgpa = sgpa;
+    await results.save();
     res.json(results);
   } catch (e) {
     res.json({ success: false, message: "Internal Server Error" });
